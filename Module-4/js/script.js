aeb62768aa705b28308617eeea842b2b36b008d7
odaialsaafani-collab <odaialsaafani@gmail.com>
2025-12-06 23:45:34 +0300

    Create script.js

---
+++ Module-4/js/script.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    var helloSpeaker = {};
+    var byeSpeaker = {};
+
+    helloSpeaker.speak = function (name) {
+        console.log("Hello " + name);
+    };
+
+    byeSpeaker.speak = function (name) {
+        console.log("Good Bye " + name);
+    };
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+
+})();
